a4d38d6db15311dc721b1c62c9c6b8e96cb6fd32
Erodozer <ero@erodozer.moe>
2022-03-29 01:19:46 -0400

    clean up event setting

--- scene/Yuuki.gd
+++ scene/Yuuki.gd
@@ -1,10 +1,33 @@
 extends "res://lib/TimeTrigger.gd"
 
 const godash = preload("res://addons/godash/godash.gd")
-
+	
 const STATES = [
-	"idle",
-	"sit"
+	{
+		"anim": "Idle",
+		"anchor": "Idle",
+		"toggle": null,
+	},
+	{
+		"anim": "Sitting",
+		"anchor": "Sitting",
+		"toggle": "toggle:sit",
+	},
+	{
+		"anim": "SittingChat",
+		"anchor": "Sitting",
+		"toggle": "toggle:sit",
+	},
+	{
+		"anim": "Idle",
+		"anchor": "Chat",
+		"toggle": null,
+	},
+	{
+		"anim": "Idle",
+		"anchor": "Counter",
+		"toggle": null,
+	}
 ]
 
 onready var sprite = get_node("AnimatedSprite")
@@ -20,13 +43,9 @@ func _on_update(_time, night):
 	for i in get_tree().get_nodes_in_group("toggle"):
 		i.visible = true
 	
-	match s:
-		"sit":
-			for i in get_tree().get_nodes_in_group("toggle:sit"):
-				i.visible = false
 		
-			sprite.animation = "Sitting"
-			global_position = anchors.get_node("Sitting").global_position
-		_:
-			sprite.animation = "Idle"
-			global_position = anchors.get_node("Idle").global_position
+	if s.toggle:
+		for i in get_tree().get_nodes_in_group(s.toggle):
+			i.visible = false
+	global_position = anchors.get_node(s.anchor).global_position	
+	sprite.animation = s.anim
